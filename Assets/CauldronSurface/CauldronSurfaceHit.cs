f29765a7d7f83cc8c1229676669015bea1cded91
Laurenth <laurenth@unity3d.com>
2019-09-27 16:54:08 +0200

    Update cauldron with new render texture. Lights and surface will react to liquid dropped

---
+++ Assets/CauldronSurface/CauldronSurfaceHit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace RDSystem
+{
+    [RequireComponent(typeof(Collider))]
+    public class CauldronSurfaceHit : MonoBehaviour
+    {
+        public RDSystem.LiquidDropUpdater updater;
+        public int materialID = 0;
+        public string colorPropertyName = "_BaseColor";
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            var pos = gameObject.transform.InverseTransformPoint(collision.contacts[0].point);
+            updater.hitPosition = new Vector2(pos.x + 0.5f, pos.y + 0.5f);
+            updater.hitColor = collision.gameObject.GetComponent<MeshRenderer>().materials[materialID].GetColor(colorPropertyName);
+            updater.Hit();
+        }
+    }
+}
---
+++ Assets/CauldronSurface/LiquidDropUpdater.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+namespace RDSystem
+{
+    public class LiquidDropUpdater : MonoBehaviour
+    {
+        [SerializeField] CustomRenderTexture _texture;
+        [SerializeField] CustomRenderTexture _textureCube;
+        [SerializeField, Range(1, 16)] int _stepsPerFrame = 4;
+        bool m_update = false;
+        bool m_hit = false;
+        public Vector2 hitPosition;
+        public Color hitColor = Color.green;
+        public float simulationDuration = 2;
+
+        void Start()
+        {
+            _texture.material.SetColor("_Color1", hitColor);
+            _texture.Initialize();
+            _textureCube.Initialize();
+        }
+
+        void Update()
+        {
+            if (m_hit == true)
+            {
+                m_update = true;
+                InitTexture(_texture);
+                InitTexture(_textureCube);
+                Invoke("Stop", simulationDuration);
+                m_hit = false;
+            }
+            else if (m_update == true)
+            {
+                UpdateTexture(_texture);
+                UpdateTexture(_textureCube);
+            }
+        }
+
+        void InitTexture(CustomRenderTexture texture)
+        {
+            texture.material.SetVector("_Position", new Vector4(hitPosition.x, hitPosition.y, 0, 0));
+            texture.material.SetColor("_Color1", hitColor);
+            texture.shaderPass = 1;
+            texture.Update();
+        }
+
+        void UpdateTexture(CustomRenderTexture texture)
+        {
+            texture.shaderPass = 0;
+            texture.Update(_stepsPerFrame);
+        }
+
+        void Stop()
+        {
+            m_update = false;
+        }
+
+        public void Hit()
+        {
+            Color liquidColor = _texture.material.GetColor("_Color1");
+            if (hitColor != liquidColor)
+                m_hit = true;
+        }
+    }
+}
