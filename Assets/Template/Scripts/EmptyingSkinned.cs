e9befd7ad4b3a4c41b0018c9795ac2b9eb151f30
SebastienLa <40639410+SebastienLa@users.noreply.github.com>
2019-09-30 15:43:47 +0200

    update

---
+++ Assets/Template/Scripts/EmptyingSkinned.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SkinnedMeshRenderer))]
+
+public class EmptyingSkinned : MonoBehaviour
+{
+
+    public float dotResult;
+    public float filling;
+
+    float newFilling;
+
+    public float speedMultiplier = 1.0f;
+    float level = 0.0f;
+
+    SkinnedMeshRenderer skinnedMeshRenderer;
+    Material material;
+
+    Vector3 vectorObj;
+    public int idMat = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+        material = skinnedMeshRenderer.materials[idMat];
+
+        filling = material.GetFloat("FillingRate");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calcul the angle to empty the tube
+        vectorObj = transform.InverseTransformVector(Vector3.up).normalized;
+        dotResult = Vector3.Dot(vectorObj, Vector3.forward);
+
+        float speed = speedMultiplier * Time.deltaTime;
+
+        if (dotResult < level)
+        {
+            //Empty the flask
+            if (filling > 0)
+            {
+                newFilling = filling - speed;
+
+                material.SetFloat("FillingRate", newFilling);
+
+                filling = newFilling;
+
+            }
+            //To correct an artefact when empty.
+            if (filling < 0.007)
+            {
+                material.SetFloat("AlphaModifier", 0.0f);
+            }
+        }
+    }
+}
