ceb2f9f3805d5f4695dc4b5fcdefb668045e343f
Thomas ICHÉ <peeweek@gmail.com>
2019-11-07 18:13:59 +0100

    Updated Gameplay Ingredients, + 1 step towards electric ark (still need to redo the VFX due to epic kludge)

---
+++ Assets/Scripts/VFX/VFXTeleportBinder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[VFXBinder("VR Demo/XR Teleport Binder")]
+public class VFXTeleportBinder : VFXBinderBase
+{
+    public XRRayInteractor XRRayInteractor;
+
+    [VFXPropertyBinding("System.Boolean")]
+    public ExposedProperty HitProperty = "IsHit";
+
+    [VFXPropertyBinding("UnityEngine.Vector3")]
+    public ExposedProperty TargetProperty = "TargetPosition";
+
+    [VFXPropertyBinding("UnityEngine.Vector3")]
+    public ExposedProperty TargetNormalProperty = "TargetNormal";
+
+    Vector3 m_Position;
+    Vector3 m_Normal;
+
+    public override bool IsValid(VisualEffect component)
+    {
+        return XRRayInteractor != null
+            && component.HasBool(HitProperty)
+            && component.HasVector3(TargetProperty)
+            && component.HasVector3(TargetNormalProperty);
+    }
+
+    public override void UpdateBinding(VisualEffect component)
+    {
+        int id = 0;
+        bool hit = false;
+
+        if (XRRayInteractor.TryGetHitInfo(ref m_Position, ref m_Normal, ref id, ref hit))
+        {
+            component.SetBool(HitProperty, true);
+            component.SetVector3(TargetProperty, m_Position);
+            component.SetVector3(TargetNormalProperty, m_Normal);
+        }
+        else
+            component.SetBool(HitProperty, false);
+    }
+}
