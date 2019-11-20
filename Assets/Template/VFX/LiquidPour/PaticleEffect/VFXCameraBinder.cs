b791070dd09fe4729c030b4b4a4a7156db07ca86
Cyril Jover <jovercyril@gmail.com>
2019-11-20 15:23:09 +0100

    Liquid VFX Features
    Adding data allowing to get depth buffer info for particle collision. Not effective yet, missing matrix 4x4 operation that Julien F is going to commit soon

---
+++ Assets/Template/VFX/LiquidPour/PaticleEffect/VFXCameraBinder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
+
+[VFXBinder("VR Demo/VFXCameraBinder")]
+public class VFXCameraBinder : VFXBinderBase
+{
+    public new Camera camera;
+    public ExposedProperty viewProjProperty = "ViewProj";
+
+    public override bool IsValid(VisualEffect component)
+    {
+        return camera != null && component != null && component.HasMatrix4x4(viewProjProperty);
+    }
+
+    public override void UpdateBinding(VisualEffect component)
+    {
+        Matrix4x4 pm = camera.projectionMatrix;
+        Matrix4x4 wcm = camera.worldToCameraMatrix;
+
+        component.SetMatrix4x4("ViewProj", pm * wcm);
+    }
+
+}
