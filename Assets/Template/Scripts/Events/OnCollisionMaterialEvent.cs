fd981499ddcf36d6894de233b4f673e3fed72c49
Laurent <l0lo86@free.fr>
2019-12-10 18:19:31 +0100

    Move table colliders inside table prefab. Assign physic material. Test cup impact sounds

---
+++ Assets/Template/Scripts/Events/OnCollisionMaterialEvent.cs
@@ -0,0 +1,29 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace GameplayIngredients.Events
+{
+    [RequireComponent(typeof(Collider))]
+    public class OnCollisionMaterialEvent : EventBase
+    {
+        [ReorderableList]
+        public Callable[] onCollisionEnter;
+
+        public bool OnlyInteractWithPhysicMaterial = true;
+        [EnableIf("OnlyInteractWithPhysicMaterial")]
+        public string PhysicMaterialName = "Wood";
+
+        private void OnCollisionEnter(Collision other)
+        {
+            //Debug.Log(other.collider.sharedMaterial.name.ToString());
+            if (OnlyInteractWithPhysicMaterial && other.collider.sharedMaterial.name == PhysicMaterialName)
+            {
+                Callable.Call(onCollisionEnter, other.collider.gameObject);
+            }
+            if (!OnlyInteractWithPhysicMaterial)
+            {
+                Callable.Call(onCollisionEnter, other.collider.gameObject);
+            }
+        }
+    }
+}
