f1fed9cb745a13b024353547b6d1f4ab73df0c98
Laurenth <laurenth@unity3d.com>
2019-12-12 19:49:59 +0100

    Iterated on audio impact scripts. First working version in the cup object

---
+++ Assets/Template/Scripts/Events/OnAudioCollisionEvent.cs
@@ -0,0 +1,19 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace GameplayIngredients.Events
+{
+    [RequireComponent(typeof(Collider))]
+    public class OnAudioCollisionEvent : EventBase
+    {
+        [ReorderableList]
+        public Callable[] onCollisionEnter;
+        [NonNullCheck]
+        public AudioImpactLogic audioImpactLogic;
+        private void OnCollisionEnter(Collision collision)
+        {
+            audioImpactLogic.SetCollision(collision);
+            Callable.Call(onCollisionEnter, collision.collider.gameObject);
+        }
+    }
+}
