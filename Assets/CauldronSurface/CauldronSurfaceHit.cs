d812aaff17437eca1639f8114b7ebd068d3f62cd
Laurent <l0lo86@free.fr>
2019-12-17 15:52:23 +0100

    Fix cauldron surface interaction, replace the mesh

--- Assets/CauldronSurface/CauldronSurfaceHit.cs
+++ Assets/CauldronSurface/CauldronSurfaceHit.cs
@@ -2,20 +2,21 @@
 
 namespace RDSystem
 {
+    [RequireComponent(typeof(Collider))]
     public class CauldronSurfaceHit : MonoBehaviour
     {
         public RDSystem.LiquidDropUpdater updater;
         public int materialID = 0;
         public string colorPropertyName = "_BaseColor";
-        public Collider m_collider;
 
         private void OnCollisionEnter(Collision collision)
         {
             var fillRate = collision.gameObject.GetComponent<MeshRenderer>().materials[materialID].GetFloat("FillingRate");
-            if(fillRate>0)
+            if(fillRate>-1)
             {
                 var pos = gameObject.transform.InverseTransformPoint(collision.contacts[0].point);
-                updater.hitPosition = new Vector2(pos.x + 0.5f, pos.y + 0.5f);
+                //Debug.Log(pos.ToString());
+                updater.hitPosition = new Vector2(-pos.x + 0.5f, pos.y + 0.5f);
                 updater.hitColor = collision.gameObject.GetComponent<MeshRenderer>().materials[materialID].GetColor(colorPropertyName);
                 updater.Hit();
             }
